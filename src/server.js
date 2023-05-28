4f05b8e7d8c45e3f3788056085a4f3fd611475ed
yaojinfeng <yaojinfeng0604@hhdd.com>
2023-05-28 11:18:01 +0800

    wx

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const Koa = require('koa');
+var Router = require('koa-router');
+
+var app = new Koa();
+var router = new Router();
+
+router.get('/wx', (ctx, next) => {
+  console.log('xx', ctx.query);
+  ctx.body = 'hello'
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+app.listen(8080, () => {
+  console.log('listening on 8080')
+})
