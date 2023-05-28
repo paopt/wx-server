8677210c51eef3b1479027b827d1cf0f3a0ce38b
yaojinfeng <yaojinfeng0604@hhdd.com>
2023-05-29 00:15:28 +0800

    静态文件处理

--- src/server.js
+++ src/server.js
@@ -1,16 +1,18 @@
 const Koa = require('koa');
 const { koaBody } = require('koa-body');
 const logger = require('koa-logger');
+const statics = require('koa-static');
+const path = require('path');
 
 const wxRouter = require('./routes/wx.route');
 const apiRouter = require('./routes/api.route');
 
 const app = new Koa();
-
 app.use(logger());
 app.use(koaBody());
 app.use(wxRouter.routes());
 app.use(apiRouter.routes());
+app.use(statics(path.join(__dirname, '../static')))
 
 app.listen(80, () => {
   console.log('listening on 80')
